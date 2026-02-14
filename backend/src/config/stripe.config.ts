b90cfe611781ca430e7eb42b1a31f0c953219a89
Faiz Ullah Khan <faizullahofficial0@gmail.com>
2026-02-14 22:14:54 +0500

    removed secret from the stripe config file

--- backend/src/config/stripe.config.ts
+++ backend/src/config/stripe.config.ts
@@ -1,12 +1,11 @@
 import Stripe from "stripe";
+import dotenv from "dotenv";
 
-const stripe = new Stripe(
-    process.env.STRIPE_SECRET_KEY ||
-        "sk_test_51SC38W36Ing01WhlP3rJXqsL2f1lhFcyb8eBED1scnCNvQ4yg5h3cctljS3rlvH2XPTMnnhAP2IEi7SOb6leGQKA00mkU1qYot",
-    {
-        apiVersion: "2026-01-28.clover",
-        typescript: true,
-    },
-);
+dotenv.config();
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: "2026-01-28.clover",
+    typescript: true,
+});
 
 export default stripe;
