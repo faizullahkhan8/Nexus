446f4f1afc7b7fae495d15c31c62ed4f8b2d05ca
Faiz Ullah Khan <faizullahofficial0@gmail.com>
2026-02-15 22:00:03 +0500

    fix mongo uri

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+    (this && this.__importDefault) ||
+    function (mod) {
+        return mod && mod.__esModule ? mod : { default: mod };
+    };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.io = void 0;
 const express_1 = __importDefault(require("express"));
@@ -14,7 +16,9 @@ const socket_io_1 = require("socket.io");
 const node_http_1 = require("node:http");
 const auth_router_1 = __importDefault(require("./routers/auth.router"));
 const request_router_1 = __importDefault(require("./routers/request.router"));
-const notification_router_1 = __importDefault(require("./routers/notification.router"));
+const notification_router_1 = __importDefault(
+    require("./routers/notification.router"),
+);
 const message_router_1 = __importDefault(require("./routers/message.router"));
 const document_router_1 = __importDefault(require("./routers/document.router"));
 const deal_router_1 = __importDefault(require("./routers/deal.router"));
@@ -39,7 +43,7 @@ const sessionMiddleware = (0, express_session_1.default)({
     name: "session",
     secret: process.env.SESSION_SECRET || "",
     store: connect_mongo_1.default.create({
-        mongoUrl: process.env.DB_URI,
+        mongoUrl: process.env.MONGO_URI_ONLINE,
         collectionName: "sessions",
         ttl: 60 * 60 * 24, // 1 day in seconds
     }),
@@ -79,8 +83,7 @@ exports.io.on("connection", (socket) => {
         console.log(reason);
     });
     const emitToUser = (event, payload) => {
-        if (!payload?.to)
-            return;
+        if (!payload?.to) return;
         exports.io.to(`user:${payload.to}`).emit(event, {
             ...payload,
             from: {
